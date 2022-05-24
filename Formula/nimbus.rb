d7702ac3c8723b06f92c2c4ae566c51d65528767
liushh <liusha@usenimbus.com>
2022-05-24 19:45:41 +0200

    Update nimbus.rb

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -1,9 +1,9 @@
 class Nimbus < Formula
   desc "Nimbus Client"
   homepage "https://usenimbus.com/"
-  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v0.1.3/nimbus-macos-x64.tar.gz"
-  sha256 "f36dcc52b4a1048f626348512e2691a57551e75b6bba53ffe3da7cd0934e0aea"
-  version "0.1.3"
+  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v0.1.4/nimbus-macos-x64.tar.gz"
+  sha256 "40179867cf7cefc3346dfb219b331d67915fb4f2449f52814022c0198c7d9a39"
+  version "0.1.4"
   def install
     bin.install "nimbus"
   end
