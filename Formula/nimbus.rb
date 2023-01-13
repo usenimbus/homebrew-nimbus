946935e1f012ff1da0b230ed6fdce5422cb3912f
Ayush Goyal <ayushg1214@gmail.com>
2023-01-13 17:37:57 +0530

    feat: Update release version

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -5,9 +5,9 @@ require 'erb'
 class Nimbus < Formula
   desc "Nimbus Client"
   homepage "https://usenimbus.com/"
-  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.0/nimbus-amd64"
-  sha256 "44aba612de3a38c3c525abb194d7a13ed1c5cb0acd1021395d0814f39382d60b"
-  version "1.0.0"
+  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.1/nimbus-1.0.1-darwin-arm64.tar.gz"
+  sha256 "be2630a7e178061b2956bea35a2d3e25b03fa8891fcc6f5c780d0e51eeae080f"
+  version "1.0.1"
   def install
     bin.install buildpath/"nimbus"
   end
