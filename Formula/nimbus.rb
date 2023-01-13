ee745728321bd7a14814b2d729858fea5b95605f
Ayush Goyal <ayushg1214@gmail.com>
2023-01-13 17:41:58 +0530

    feat: Arm and amd releases

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -5,11 +5,25 @@ require 'erb'
 class Nimbus < Formula
   desc "Nimbus Client"
   homepage "https://usenimbus.com/"
-  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.1/nimbus-1.0.1-darwin-arm64.tar.gz"
-  sha256 "be2630a7e178061b2956bea35a2d3e25b03fa8891fcc6f5c780d0e51eeae080f"
   version "1.0.1"
-  def install
-    bin.install buildpath/"nimbus"
+
+
+  if Hardware::CPU.arm?
+    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.1/nimbus-1.0.1-darwin-arm64.tar.gz"
+    sha256 "be2630a7e178061b2956bea35a2d3e25b03fa8891fcc6f5c780d0e51eeae080f"
+
+    def install
+      bin.install buildpath/"nimbus"
+    end
+  end
+
+  if Hardware::CPU.intel?
+    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.1/nimbus-1.0.1-darwin-amd64.tar.gz"
+    sha256 "5c975760c4237c7219b626d4edf239451724ed4858aaef30aef7bc6c4f733d46"
+  
+    def install
+      bin.install buildpath/"nimbus"
+    end
   end
   
   test do
