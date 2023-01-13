9b13515e21262cffbdc8bedc5852c3b764466ee7
Ayush Goyal <ayushg1214@gmail.com>
2023-01-13 11:35:04 +0530

    release: amd release

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -1,9 +1,9 @@
 class Nimbus < Formula
   desc "Nimbus Client"
   homepage "https://usenimbus.com/"
-  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v0.1.4/nimbus-macos-x64.tar.gz"
-  sha256 "40179867cf7cefc3346dfb219b331d67915fb4f2449f52814022c0198c7d9a39"
-  version "0.1.4"
+  url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.0/nimbus-amd64"
+  sha256 "44aba612de3a38c3c525abb194d7a13ed1c5cb0acd1021395d0814f39382d60b"
+  version "1.0.0"
   def install
     bin.install "nimbus"
   end
