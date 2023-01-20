35a6ebe26edda246bf1e18d86f158e45b748a80c
Ayush Goyal <ayushg1214@gmail.com>
2023-01-20 14:58:28 +0530

    build: bump version

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -9,7 +9,7 @@ class Nimbus < Formula
 
 
   if Hardware::CPU.arm?
-    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.3/nimbus-1.0.3-darwin-arm64.tar.gz"
+    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.4/nimbus-1.0.4-darwin-arm64.tar.gz"
     sha256 "11c1f4b8774235189ffed6c3f8abeb6751d63ca6c1aa1cbf4602cdaeda6e3cd6"
 
     def install
@@ -18,7 +18,7 @@ class Nimbus < Formula
   end
 
   if Hardware::CPU.intel?
-    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.3/nimbus-1.0.3-darwin-amd64.tar.gz"
+    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.4/nimbus-1.0.4-darwin-amd64.tar.gz"
     sha256 "2b367cc9d76559d8d18db02acaa630a0ee14406a949d94a2f9877f80a60d308a"
   
     def install
