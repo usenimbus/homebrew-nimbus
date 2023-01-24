4cb96d0bcb617807c79474fa2ae70709b9cf1dac
Ayush Goyal <ayushg1214@gmail.com>
2023-01-25 02:32:59 +0530

    chore: bump version

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -5,12 +5,12 @@ require 'erb'
 class Nimbus < Formula
   desc "Nimbus Client"
   homepage "https://usenimbus.com/"
-  version "1.0.4"
+  version "1.0.5"
 
 
   if Hardware::CPU.arm?
-    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.4/nimbus-1.0.4-darwin-arm64.tar.gz"
-    sha256 "11c1f4b8774235189ffed6c3f8abeb6751d63ca6c1aa1cbf4602cdaeda6e3cd6"
+    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.5/nimbus-1.0.5-darwin-arm64.tar.gz"
+    sha256 "02e89736e2b80dc16f0cc973b37e801f2f4a532bb011dd6a58188e30c9017f49"
 
     def install
       bin.install buildpath/"nimbus"
@@ -18,8 +18,8 @@ class Nimbus < Formula
   end
 
   if Hardware::CPU.intel?
-    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.4/nimbus-1.0.4-darwin-amd64.tar.gz"
-    sha256 "2b367cc9d76559d8d18db02acaa630a0ee14406a949d94a2f9877f80a60d308a"
+    url "https://github.com/usenimbus/homebrew-nimbus/releases/download/v1.0.5/nimbus-1.0.5-darwin-amd64.tar.gz"
+    sha256 "7a0b65193d280272f03d3896b14c0669db2fdb105771285b43aa423e90f91ee5"
   
     def install
       bin.install buildpath/"nimbus"
