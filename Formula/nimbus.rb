c4f48138e289a9b9a3b94e09e68c389f8f9a081b
Ayush Goyal <ayushg1214@gmail.com>
2023-02-22 19:05:12 +0530

    build: bump to 1.0.10

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -5,12 +5,12 @@ require 'erb'
 class Nimbus < Formula
   desc "Nimbus Client"
   homepage "https://usenimbus.com/"
-  version "1.0.9"
+  version "1.0.10"
 
 
   if Hardware::CPU.arm?
-    url "https://d1sn0nbc82p4fc.cloudfront.net/nimbus-cli/cli_v1.0.9/nimbus-1.0.9-darwin-arm64.tar.gz"
-    sha256 "9fa0ae2c782643bcaa6623cb92615fefd4b2ddfd3be7a701e731089ed728453e"
+    url "https://d1sn0nbc82p4fc.cloudfront.net/nimbus-cli/cli_v1.0.10/nimbus-1.0.10-darwin-arm64.tar.gz"
+    sha256 "8584978ecd5e57241b9d6ee44e2bc857ff6002ef91c72c1b232b1ed4b181aa3e"
 
     def install
       bin.install buildpath/"nimbus"
@@ -18,8 +18,8 @@ class Nimbus < Formula
   end
 
   if Hardware::CPU.intel?
-    url "https://d1sn0nbc82p4fc.cloudfront.net/nimbus-cli/cli_v1.0.9/nimbus-1.0.9-darwin-amd64.tar.gz"
-    sha256 "2678f0d837d1db86307f89c6e4ed18092d26bfcbd5040b0b827047919946d455"
+    url "https://d1sn0nbc82p4fc.cloudfront.net/nimbus-cli/cli_v1.0.10/nimbus-1.0.10-darwin-amd64.tar.gz"
+    sha256 "a0fafcaeb3cbf233b12c394a49d50d664a4b889aac6299468e4962c020779bae"
   
     def install
       bin.install buildpath/"nimbus"
